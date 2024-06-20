ae459d11346764d2e90d65f26522802ca2c394fd
nicosup <damiconicola98@gmail.com>
2024-06-20 10:10:20 -0400

    first commit

---
+++ database/migrations/2024_05_23_155643_todo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("Todo", function(Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->tinyText('description');
+            $table->enum('state',['pending','doing','done']);
+            $table->bigInteger('user_id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
